243b0a45d1ebb1be528cb845b692afe6ebed2571
rangapv <rangapv@yahoo.com>
2025-07-20 18:33:06 +0530

    Initial script to check pre-reqs

---
+++ basicinstall.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env zsh
+#author:rangapv@yahoo.com
+#20-07-25
+
+source <(curl -s https://raw.githubusercontent.com/rangapv/bash-source/main/s1.sh) >>/dev/null 2>&1
+source <(curl -s https://raw.githubusercontent.com/rangapv/ansible-install/refs/heads/main/pyverchk.sh) >>/dev/null 2>&1
+
+
+#echo "$mac"
+#echo "$una"
+
+if [ ! -z "$mac" ]
+then
+	echo "It is a Mac"
+	cm1="brew"
+	count=1
+else
+	echo "This script is for MacOS  distribution only"
+        exit
+fi
+
+pyvercheck python3
+
+if [ "$pyuni" = "python3" ]
+then
+    echo "Then installed Pytohn on this Mac is $pyuni with the version $piver1"
+else
+    echo "No python found"
+fi
