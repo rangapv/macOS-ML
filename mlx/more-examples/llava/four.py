4477a4887c2c54acb195034461f64ea546bb4193
rangapv <rangapv@yahoo.com>
2026-04-28 12:18:59 +0530

    Worked perfectly for USER/ASSIST

---
+++ mlx/more-examples/llava/four.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+#author:rangapv@yahoo.com
+#27-04-26
+
+
+from PIL import Image
+from transformers import AutoProcessor, LlavaForConditionalGeneration
+
+model = LlavaForConditionalGeneration.from_pretrained("llava-hf/llava-1.5-7b-hf")
+processor = AutoProcessor.from_pretrained("llava-hf/llava-1.5-7b-hf")
+
+def mesg1():
+   input1 = input("USER:")
+   m2 = [
+       {
+        "role": "user",
+        "content": [
+            {"type": "image", "url": "https://static01.nyt.com/images/2023/07/21/multimedia/21baguettesrex-hbkc/21baguettesrex-hbkc-videoSixteenByNineJumbo1600.jpg"},
+            {"type": "text", "text": input1}
+        ]   
+       }
+   ]   
+  # m1 = message.append(m2)   
+   m1 = m2
+   #print(f"inside mesg1 {m1}")
+   #message = m1
+   return m1
+   
+while True:
+ message = mesg1()
+ #print(f"inside while {message}")
+ inputs = processor.apply_chat_template(
+    message,
+    tokenize=True,
+    return_dict=True,
+    return_tensors="pt",
+    add_generation_prompt=True
+    #continue_final_message=True
+ )
+# Generate
+ #generate_ids = model.generate(**inputs)
+ generate_ids = model.generate(**inputs,max_new_tokens=625)
+ result = processor.decode(generate_ids, skip_special_tokens=True)
+ #print(f"result is {result}")
+ result1 = result[0]
+ #print(f"result1 is {result1}")
+ w1 = "ASSISTANT:"
+ indx = result1.find(w1)
+ result2 = result1[indx:]
+ #print(f"result2 is {result2}")
+ print(result2)
